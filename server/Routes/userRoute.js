2dcc5d6ea83a8d3af8d522ba3b668d49a4340d2f
lindiar.m <lindiar.m@outlook.com>
2020-12-31 10:03:53 +0100

    commit:Issue-2 #2

--- server/Routes/userRoute.js
+++ server/Routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require("../Controller/userController");
-router.post("/", userController.decodeAdress );
+router.post("/reverse", userController.decodeAdress );
 
 
 module.exports = router;
