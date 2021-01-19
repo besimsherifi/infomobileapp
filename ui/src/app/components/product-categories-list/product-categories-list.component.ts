67fe042d3fb8f4a9707a662d3fbdaa37154e8f88
lindiar.m <lindiar.m@outlook.com>
2021-01-19 14:52:58 +0100

    commit:Issue-4 #4

---
+++ ui/src/app/components/product-categories-list/product-categories-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-categories-list',
+  templateUrl: './product-categories-list.component.html',
+  styleUrls: ['./product-categories-list.component.css']
+})
+export class ProductCategoriesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
