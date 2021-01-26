11d1d2b859961ce5ffb52087678238d51efd75d7
edon.iseini <edon.iseini@i-solve.info>
2021-01-26 14:17:05 +0100

    commit:Issue-5 #5

--- ui/src/app/components/productdetails/productdetails.component.ts
+++ ui/src/app/components/productdetails/productdetails.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { DataService } from '../../data.service';
+import { switchMap, map, catchError } from 'rxjs/operators';
+import { Product } from 'src/app/models/product.model';
+
 
 @Component({
   selector: 'app-productdetails',
@@ -10,6 +13,8 @@ import { DataService } from '../../data.service';
 export class ProductdetailsComponent implements OnInit {
   products: any = [];
   imgpath = 'https://localhost:44364/ProductsImages/';
+  id;
+
   constructor(
     public dataservice: DataService,
     private route: ActivatedRoute,
@@ -18,21 +23,43 @@ export class ProductdetailsComponent implements OnInit {
 
   // tslint:disable-next-line:typedef
   ngOnInit() {
-    this.getProduct(this.route.snapshot.paramMap.get('id'));
+    this.route.paramMap
+      .pipe(
+        switchMap(params => {this.id = +params.get('id');
+
+                             return this.getProduct(+params.get('id'));
+        }
+        ))
+      .subscribe((data) => {this.products = data,
+
+        console.log(data, 'Produkteteeeeee');
+                            if (this.products.length < 1) {
+                              this.router.navigate(['/detail', this.id - 1]);
+                             }
+      },
+    (error) => {
+      console.log(error);
+    });
   }
+
+
+  // tslint:disable-next-line:typedef
+
+
+
   // tslint:disable-next-line:typedef
   getProduct(id) {
-    this.dataservice.getProductsbyID(id).subscribe(
-      (data) => {
-        this.products = data;
-        console.log(data, 'curentproduct');
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
+  return this.dataservice.getProductsbyID(id);
   }
 
 
+
+  // tslint:disable-next-line:typedef
+  goNext(){
+
+    console.log(this.id , 'ProoddddIDD');
+    this.router.navigate(['/detail', this.id + 1]);
+
+  }
 
 }
