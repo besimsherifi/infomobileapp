720fcf4f057254bdc6f39330813aa5b3fd690012
edon.iseini <edon.iseini@i-solve.info>
2021-03-04 13:29:27 +0100

    Commit:Issue-14 #14 *Vendosja e alertit nese produkti nuk ekziston si favorit*

--- ui/src/app/components/favorites/favorites.component.ts
+++ ui/src/app/components/favorites/favorites.component.ts
@@ -28,9 +28,23 @@ export class FavoritesComponent implements OnInit {
         }
       })
     }
-    this.data = JSON.parse(localStorage.getItem('fav'));
-    this.searchService.searchTextt.subscribe((val) => {
-      this.searchTextt = val;
-    });
+    else if(localStorage.getItem('fav') == '[]') {
+      Swal.fire({
+        text: 'Favorite product not found',
+        icon: 'warning',
+        confirmButtonText: 'Ok!'
+      }).then((result) => {
+        if(result.isConfirmed) {
+          this.router.navigate(['/']);
+        }
+      })
+    }
+    else{
+      this.data = JSON.parse(localStorage.getItem('fav'));
+      this.searchService.searchTextt.subscribe((val) => {
+        this.searchTextt = val;
+      });
+    }
+
   }
 }
