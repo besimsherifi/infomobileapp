65b25f562e94bc193fc1dfabcb297667c74d39f3
lindiar.m <lindiar.m@outlook.com>
2021-03-09 15:16:56 +0100

    commit:Issue-15 #15

--- ui/src/app/components/companies/companies.component.ts
+++ ui/src/app/components/companies/companies.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/data.service';
 import { Options } from '@angular-slider/ngx-slider';
 import { SearchService } from '../../search.service';
-
+import * as geolib from 'geolib';
 @Component({
   selector: 'app-companies',
   templateUrl: './companies.component.html',
@@ -15,11 +15,10 @@ export class CompaniesComponent implements OnInit {
     private dataService: DataService,
     private searchService: SearchService
   ) {}
-
   compani: any = [];
+  // imgpath = 'https://localhost:44364/';
   imgpath = 'https://develop.conome.mk/';
   companies: any = [];
-
   // slider rangekm
   value: number = 25;
   options: Options = {
@@ -31,36 +30,43 @@ export class CompaniesComponent implements OnInit {
       { value: 200, legend: 'km' },
     ],
   };
-
   detectchange(value) {
     this.findMe();
   }
-
   ngOnInit() {
     this.searchService.searchTextt.subscribe((val) => {
       this.searchTextt = val;
     });
     this.findMe();
   }
-
   findMe() {
     let decodedData;
-    if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
-        const data = {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          radius: this.value * 1000,
-        };
-        this.dataService.getCrmCompaniesByUserAddress(data).subscribe(
+        this.dataService.getCrmCompaniesByUserAddress().subscribe(
           (formated) => {
             this.companies = formated;
+            this.compani = [];
+            this.companies.forEach(formate => {
+              formate.addresses.forEach(adress => {
+                const latitudee = adress.latLng.lat;
+                const longitudee = adress.latLng.lng;
+                const radius = this.value * 1000;
+                var latitude = position.coords.latitude;
+                var longitude = position.coords.longitude;
+                var geolibi = geolib.isPointWithinRadius(
+                  { latitude: latitude, longitude: longitude },
+                  { latitude: latitudee, longitude: longitudee },
+                  radius //meters
+                );
+                if(geolibi){
+                this.compani.push(formate)
+            }
+            });
+          });
             console.log(this.companies, 'Companya');
           },
           (error) => {}
         );
       });
-    } else {
     }
-  }
 }
--- ui/src/app/data.service.ts
+++ ui/src/app/data.service.ts
@@ -1,4 +1,3 @@
-import { ICompany } from './interfaces/ICompany';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
@@ -11,34 +10,32 @@ import { IProduct } from './interfaces/IProduct';
 export class DataService {
   constructor(private http: HttpClient, private router: Router) {}
 
-  getCrmCompaniesByUserAddress(data): Observable<any> {
-    // console.log(data, 'data');
-    return this.http.post('http://localhost:8000/api/crm/c', data);
+  getCrmCompaniesByUserAddress(): Observable<any> {
+    return this.http.get<any>('https://develop.conome.mk/api/companyapi/index');
   }
 
 
   getcompaniesbyID(id): Observable<any> {
     return this.http.get<IProduct>(
-      'https://localhost:44364/api/companyapi/compdetails',{ params: { id } }
+      'https://develop.conome.mk/api/companyapi/compdetails',{ params: { id } }
     );
   }
 
-  // getAllProducts(): Observable<any> {
-  //   // console.log(data, 'data');
-  //   return this.http.get<any>(
-  //     'https://develop.conome.mk/api/companyapi/allproducts'
-  //   );
-  // }
+  getAllProducts(): Observable<any> {
+    return this.http.get<any>(
+      'https://develop.conome.mk/api/companyapi/allproducts'
+    );
+  }
 
   getProductsbyID(id): Observable<any> {
     return this.http.get<IProduct>(
-      'https://localhost:44364/api/companyapi/details',{ params: { id } }
+      'https://develop.conome.mk/api/companyapi/details',{ params: { id } }
     );
   }
 
   getProducts(): Observable<any> {
     return this.http.get<IProduct>(
-      'https://localhost:44364/api/companyapi/product'
+      'https://develop.conome.mk/api/companyapi/product'
     );
   }
 }
